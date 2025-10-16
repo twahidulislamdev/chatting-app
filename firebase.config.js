babb64a6bcf48fcfb768ebd6e72e0f70f61182e5
Twahidul Islam <twahidulislam2005@gmail.com>
2025-10-16 13:51:08 +0600

    feat: add Firebase integration and configuration
    - Added Firebase as a dependency in package.json.
    - Imported Firebase configuration in main.jsx.
    - Created firebase.config.js to initialize Firebase with the provided configuration.

---
+++ firebase.config.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAH08a3d6HfSpvnWBqL4lRMYUm0AzZke4",
+  authDomain: "chattingapp-b5a19.firebaseapp.com",
+  projectId: "chattingapp-b5a19",
+  storageBucket: "chattingapp-b5a19.firebasestorage.app",
+  messagingSenderId: "1068311334219",
+  appId: "1:1068311334219:web:e559cc41cdd577fe966cce",
+  measurementId: "G-DM4H7672PL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default firebaseConfig
